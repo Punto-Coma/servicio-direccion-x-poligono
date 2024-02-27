5ab5319014680811d32df89ad0f2bb00eac64c49
f-antonelli <antonelli.fd@gmail.com>
2024-02-27 17:21:15 -0300

    feat: new requirement-based endpoint

--- app.js
+++ app.js
@@ -8,16 +8,57 @@ const PORT = 3001;
 const app = express();
 app.use(express.json());
 
-app.post("/check-address", async (req, res, next) => {
-  const address = req.body.address;
-  if (!req.body.address) throw new Error("No ingresaste una direccion");
-
-  const geocoderOptions = {
-    provider: "openstreetmap",
-  };
-  const geocoder = NodeGeocoder(geocoderOptions);
-
+app.post("/check_address", async (req, res, next) => {
   try {
+    const { address, pointPolygon } = req.body;
+    if (!address || !pointPolygon) {
+      throw new Error("No ingresaste una direccion o el poligono");
+    }
+
+    const geocoderOptions = {
+      provider: "openstreetmap",
+    };
+    const geocoder = NodeGeocoder(geocoderOptions);
+
+    const geocoded = await geocoder.geocode(address);
+
+    if (!geocoded || geocoded.length === 0) {
+      throw new Error("No se pudo encontrar la direccion");
+    }
+
+    const point = turf.point([geocoded[0].longitude, geocoded[0].latitude]);
+
+    const isValidAddress = turf.booleanPointInPolygon(
+      point,
+      turf.polygon([pointPolygon])
+    );
+
+    if (isValidAddress)
+      return res.status(200).json({
+        esta_dentro: isValidAddress,
+      });
+
+    if (!isValidAddress)
+      return res.status(400).json({
+        esta_dentro: isValidAddress,
+        message: "No se encuentra dentro del poligono",
+      });
+  } catch (error) {
+    console.log("ERROR: ", error);
+    return res.status(400).json({ message: error.message });
+  }
+});
+
+app.post("/check_address-v2", async (req, res, next) => {
+  try {
+    const address = req.body.address;
+    if (!req.body.address) throw new Error("No ingresaste una direccion");
+
+    const geocoderOptions = {
+      provider: "openstreetmap",
+    };
+    const geocoder = NodeGeocoder(geocoderOptions);
+
     const geocoded = await geocoder.geocode(address);
 
     if (!geocoded || geocoded.length === 0) {
