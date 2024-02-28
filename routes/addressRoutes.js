4fa53075bc70a996358a2be56d943d77a393c14f
NicoLopezBjj <lopeznnr@gmail.com>
2024-02-28 14:32:06 -0300

    changes name index.js and addressRoutes.js

--- routes/addressRoutes.js
+++ routes/addressRoutes.js
@@ -1,12 +1,7 @@
 const router = require("express").Router();
-const NodeGeocoder = require("node-geocoder");
-const turf = require("@turf/turf");
-
-const polygons = require("../data/polygons.json");
 const controllers = require("../controllers/addressControllers")
 
 router.post("/area", controllers.searchArea)
-
 router.post("/city", controllers.searchAddress)
 
 module.exports = router;
